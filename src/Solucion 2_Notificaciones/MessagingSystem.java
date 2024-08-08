9a5c6f74bd2f1985d7dccc51dd1136a7cffe1239
Sebastian Piñeros <sebas.picas08@gmail.com>
2024-08-08 10:42:22 -0500

    Creacion Observer y Sistema de notificaiones

---
+++ src/Solucion 2_Notificaciones/MessagingSystem.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessagingSystem {
+
+    private List<Observer> observers;
+
+    public MessagingSystem(){
+        this.observers = new ArrayList<>();
+    }
+
+    public void addDevice(Observer observer){
+        this.observers.add(observer);
+    }
+
+    public void removeDevice(Observer observer){
+        this.observers.remove(observer);
+    }
+
+    public void notifyDevice(String message){
+        for (Observer observer : this.observers) {
+            observer.update();
+        }
+    }
+
+    public void setEstado(String message) {
+        System.out.println("Mensaje recibido: " + message);
+        notifyDevice(message);
+    }
+}
